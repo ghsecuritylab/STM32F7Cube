3ac08a7272446021a23d34fe8812d439a2b88472
colinporth <colinporth@users.noreply.github.com>
2018-02-21 09:10:31 +0000

    split out c stuff from utils

--- Projects/STM32746G-Discovery/Applications/USB_Device/AUDIO_Standalone/SW4STM32/STM32746G-DISCOVERY_USBD-FS/Debug/app/subdir.mk
+++ Projects/STM32746G-Discovery/Applications/USB_Device/AUDIO_Standalone/SW4STM32/STM32746G-DISCOVERY_USBD-FS/Debug/app/subdir.mk
@@ -9,28 +9,31 @@ C://projects/STM32F7Cube/Projects/STM32746G-Discovery/Applications/USB_Device/AU
 C_SRCS += \
 C://projects/STM32F7Cube/Projects/STM32746G-Discovery/Applications/stm32746g_discovery_audio.c \
 C://projects/STM32F7Cube/Projects/STM32746G-Discovery/Applications/stm32746g_discovery_lcd.c \
-C://projects/STM32F7Cube/Projects/STM32746G-Discovery/Applications/utils.c \
+C:/projects/STM32F7Cube/Projects/STM32746G-Discovery/Applications/system.c \
 C://projects/STM32F7Cube/Projects/STM32746G-Discovery/Applications/wm8994.c 
 
 CPP_SRCS += \
-C:/projects/STM32F7Cube/Projects/STM32746G-Discovery/Applications/USB_Device/AUDIO_Standalone/Src/main.cpp 
+C:/projects/STM32F7Cube/Projects/STM32746G-Discovery/Applications/USB_Device/AUDIO_Standalone/Src/main.cpp \
+C:/projects/STM32F7Cube/Projects/STM32746G-Discovery/Applications/utils.cpp 
 
 OBJS += \
 ./app/main.o \
 ./app/startup_stm32f746xx.o \
 ./app/stm32746g_discovery_audio.o \
 ./app/stm32746g_discovery_lcd.o \
+./app/system.o \
 ./app/utils.o \
 ./app/wm8994.o 
 
 C_DEPS += \
 ./app/stm32746g_discovery_audio.d \
 ./app/stm32746g_discovery_lcd.d \
-./app/utils.d \
+./app/system.d \
 ./app/wm8994.d 
 
 CPP_DEPS += \
-./app/main.d 
+./app/main.d \
+./app/utils.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
@@ -66,11 +69,19 @@ app/stm32746g_discovery_lcd.o: C://projects/STM32F7Cube/Projects/STM32746G-Disco
 	@echo 'Finished building: $<'
 	@echo ' '
 
-app/utils.o: C://projects/STM32F7Cube/Projects/STM32746G-Discovery/Applications/utils.c
+app/system.o: C:/projects/STM32F7Cube/Projects/STM32746G-Discovery/Applications/system.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU GCC Compiler'
 	@echo $(PWD)
 	arm-none-eabi-gcc -mcpu=cortex-m7 -mthumb -mfloat-abi=hard -mfpu=fpv5-sp-d16 -std=c11 -DUSE_HAL_DRIVER -DSTM32F746xx -DUSE_STM32746G_DISCO -I"C:/projects/STM32F7Cube/Middlewares/ST/STM32_USB_Device_Library/Core/Inc" -I"C:/projects/STM32F7Cube/Middlewares/ST/STM32_USB_Device_Library/Class/AUDIO/Inc" -I"C:/projects/STM32F7Cube/Drivers/STM32F7xx_HAL_Driver/Inc" -I"C:/projects/STM32F7Cube/Drivers/BSP/STM32746G-Discovery" -I"C:/projects/STM32F7Cube/Drivers/CMSIS/Device/ST/STM32F7xx/Include" -I"C:/projects/STM32F7Cube/Drivers/CMSIS/Include" -I"C:/projects/STM32F7Cube/Projects/STM32746G-Discovery/Applications/USB_Device/AUDIO_Standalone/Src"  -O2 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+app/utils.o: C:/projects/STM32F7Cube/Projects/STM32746G-Discovery/Applications/utils.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU G++ Compiler'
+	@echo $(PWD)
+	arm-none-eabi-g++ -mcpu=cortex-m7 -mthumb -mfloat-abi=hard -mfpu=fpv5-sp-d16 -DUSE_HAL_DRIVER -DSTM32F746xx -DUSE_STM32746G_DISCO -I"C:/projects/STM32F7Cube/Middlewares/ST/STM32_USB_Device_Library/Core/Inc" -I"C:/projects/STM32F7Cube/Middlewares/ST/STM32_USB_Device_Library/Class/AUDIO/Inc" -I"C:/projects/STM32F7Cube/Drivers/STM32F7xx_HAL_Driver/Inc" -I"C:/projects/STM32F7Cube/Drivers/BSP/STM32746G-Discovery" -I"C:/projects/STM32F7Cube/Drivers/CMSIS/Device/ST/STM32F7xx/Include" -I"C:/projects/STM32F7Cube/Drivers/CMSIS/Include" -I"C:/projects/STM32F7Cube/Projects/STM32746G-Discovery/Applications/USB_Device/AUDIO_Standalone/Src"  -O2 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fno-exceptions -fno-rtti -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
---
+++ Projects/STM32746G-Discovery/Applications/system.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+#include "stm32f7xx_hal.h"
+#include "stm32746g_discovery_lcd.h"
+#include "stm32746g_discovery_ts.h"
+
+//{{{
+#ifdef __cplusplus
+  extern "C" {
+#endif
+//}}}
+//{{{
+#ifdef __cplusplus
+}
+#endif
+//}}}
