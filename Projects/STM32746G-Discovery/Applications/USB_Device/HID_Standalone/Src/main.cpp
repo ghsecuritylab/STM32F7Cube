34692545a5b1c165732ed3d98318ac38cfd38c71
colinporth <colinporth@users.noreply.github.com>
2018-02-28 09:41:59 +0000

    bettrer hid report

--- Projects/STM32746G-Discovery/Applications/USB_Device/HID_Standalone/Src/main.cpp
+++ Projects/STM32746G-Discovery/Applications/USB_Device/HID_Standalone/Src/main.cpp
@@ -82,21 +82,22 @@ __ALIGN_BEGIN uint8_t kHidReportDescriptor[] __ALIGN_END = {
   0xA1, 0x01,  // Collection (Application)
     //0x85, 0x01,  //  Report ID (1)  - extra byte in report with id = 1
     0x05, 0x07,  //  Usage Page (Keyboard/Keypad)
-    0x75, 0x01,  //  Report Size (1)
-    0x95, 0x08,  //  Report Count (8)
-    0x19, 0xE0,  //  Usage Minimum (0xE0)
-    0x29, 0xE7,  //  Usage Maximum (0xE7)
-    0x15, 0x00,  //  Logical Minimum (0)
-    0x25, 0x01,  //  Logical Maximum (1)
-    0x81, 0x02,  //  Input (Data, Var, Abs, No Wrap,Linear,Preferred State,No Null Position)
-    0x95, 0x06,  //  Report Count
-    0x75, 0x08,  //  Report Size (8)
-    0x15, 0x00,  //  Logical Minimum (0)
-    0x25, 0x64,  //  Logical Maximum (100)
+    0x75, 0x01,  //  Report Size - 1 bit
+    0x95, 0x08,  //  Report Count - 8
+    0x15, 0x00,  //  Logical Minimum - 0
+    0x25, 0x01,  //  Logical Maximum - 1
+    0x19, 0xE0,  //  Usage Minimum - 0xE0
+    0x29, 0xE7,  //  Usage Maximum - 0xE7
+    0x81, 0x02,  //  Input - Data, Var, Abs, No Wrap,Linear,Preferred State,No Null Position
+
+    0x75, 0x08,  //  Report Size - 8 bits
+    0x95, 0x06,  //  Report Count - 6
+    0x15, 0x00,  //  Logical Minimum - 0
+    0x25, 0x64,  //  Logical Maximum - 100
     0x05, 0x07,  //  Usage Page (Keyboard/Keypad)
-    0x19, 0x00,  //  Usage Minimum (0x00)
-    0x29, 0x65,  //  Usage Maximum (0x65)
-    0x81, 0x00,  //  Input (Data, Array, Abs, No Wrap,Linear,Preferred State,No Null Position)
+    0x19, 0x00,  //  Usage Minimum - 0x00
+    0x29, 0x65,  //  Usage Maximum - 101
+    0x81, 0x00,  //  Input - Data, Array, Abs, No Wrap,Linear,Preferred State,No Null Position
   0xC0,        // End Collection
   };
 //}}}
@@ -390,19 +391,19 @@ void hidSendMouse (USBD_HandleTypeDef* device, uint8_t* report) {
 //{{{
 void hidSendKeyboard (uint8_t modifier, uint8_t code) {
 
-  struct keyboardHID_t {
-    //uint8_t id;
-    uint8_t mModifiers;
-    uint8_t mKey1;
-    uint8_t mKey2;
-    uint8_t mKey3;
-    uint8_t mKey4;
-    uint8_t mKey5;
-    uint8_t mKey6;
+  class cKeyboardHID {
+  public:
+    //uint8_t mId = 1;
+    uint8_t mModifiers = 0;
+    uint8_t mKey1 = 0;
+    uint8_t mKey2 = 0;
+    uint8_t mKey3 = 0;
+    uint8_t mKey4 = 0;
+    uint8_t mKey5 = 0;
+    uint8_t mKey6 = 0;
     };
-  struct keyboardHID_t keyboardHID;
 
-  //keyboardHID.id = 1;
+  cKeyboardHID keyboardHID;
   keyboardHID.mModifiers = modifier;
   keyboardHID.mKey1 = 0;
   keyboardHID.mKey2 = 0;
